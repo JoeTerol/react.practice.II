edb8f8fee58b698cd58bdac24dd8f06130fd48de
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-29 20:09:35 -0300

    html component ready

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img
+          alt="el avatar boy"
+          src="https://cdn-icons-png.flaticon.com/512/3532/3532843.png"
+        />
+        <div>
+          <strong>Jose Terol</strong>
+          <span>JoeTerol</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
